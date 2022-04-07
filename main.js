3df56bb5e163e5ce4046c4b5148e113fba468c28
stefanocravotta <stefano.cravotta8@gmail.com>
2022-04-07 15:19:15 +0200

    pulizia

--- main.js
+++ main.js
@@ -1,11 +1,8 @@
 
 
 const hamburgerMenu = document.querySelector('.header-right > a');
-console.log(hamburgerMenu);
 const hamburgerMenuContent = document.querySelector('.hamburger-menu');
-console.log(hamburgerMenuContent);
 const exitItem = document.querySelector('.close');
-console.log(exitItem);
 
 
 hamburgerMenu.addEventListener('click', function(){
